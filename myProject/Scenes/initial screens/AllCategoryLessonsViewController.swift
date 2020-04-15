9fd1bcce7029922b2eaa7761e271f1d95f4835df
rohan199719 <monubelwatika@gmail.com>
2020-04-15 21:16:37 +0530

    add category screen and swift file

---
+++ myProject/Scenes/initial screens/AllCategoryLessonsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AllCategoryLessonsViewController.swift
+//  myProject
+//
+//  Created by user161118 on 4/15/20.
+//  Copyright © 2020 user161118. All rights reserved.
+//
+
+import UIKit
+
+class AllCategoryLessonsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myProject/Scenes/initial screens/HomeScreenVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeScreenVC.swift
+//  myProject
+//
+//  Created by user161118 on 4/15/20.
+//  Copyright © 2020 user161118. All rights reserved.
+//
+
+import UIKit
+
+class HomeScreenVC: UIViewController {
+
+    @IBAction func imagePageControl(_ sender: Any) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
